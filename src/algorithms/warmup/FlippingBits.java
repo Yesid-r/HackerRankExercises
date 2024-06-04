9f576b711bcc4963e5f702b72d8be87cefce962f
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-06-03 20:52:44 -0500

    add exercise flipping bits

---
+++ src/algorithms/warmup/FlippingBits.java
@@ -0,0 +1,50 @@
+package algorithms.warmup;
+
+public class FlippingBits {
+    public static long flippingBits(long n) {
+        // Write your code here
+
+
+        String numberTobinary = convertToBinary(n, 2);
+
+        String flipeed = "";
+        for (int i = 0; i < 32; i++) {
+            if( i< numberTobinary.length() && numberTobinary.charAt(numberTobinary.length()-(1+i)) == '1'){
+                flipeed += 0;
+            } else {
+                flipeed +=1;
+            }
+        }
+        String invertido = new StringBuilder(flipeed).reverse().toString();
+        //return convertToDecimal(invertido);
+        //Flip the bits using bitwise NOT operator and mask with 0xFFFFFFFFL to keep it 32-bit unsigned
+        return ~n & 0xFFFFFFFFL;
+    }
+
+    public static String convertToBinary (long number, int base){
+        String result = "";
+        long aux = number;
+        while (aux > 0){
+            if(aux%base == 0){
+                result += "0";
+            }else {
+                result += "1";
+            }
+            aux = aux/2;
+        }
+        return result;
+    }
+
+    public static long convertToDecimal (String number){
+        long result = 0L;
+        for (int i = 0; i < number.length(); i++) {
+            if(number.charAt(number.length()-(1+i)) == '1'){
+                result += Math.pow(2, i);
+            }
+        }
+
+        return result;
+    }
+
+
+}
