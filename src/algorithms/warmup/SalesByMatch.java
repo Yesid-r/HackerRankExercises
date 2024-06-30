f5f70de76f8d9324603aa8ce8447cc6f80f95e4a
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-06-29 20:20:03 -0500

    refactor sales by match

--- src/algorithms/warmup/SalesByMatch.java
+++ src/algorithms/warmup/SalesByMatch.java
@@ -18,19 +18,17 @@ public class SalesByMatch {
         3
      */
     public static int sockMerchant(int n, List<Integer> ar) {
-        // Write your code here
-        HashMap<Integer, Integer> group = new HashMap<Integer, Integer>();
-        for (int i = 0; i < ar.size(); i++) {
-            if(group.get(ar.get(i)) == null){
-                group.put(ar.get(i), 1);
-            }else{
-                group.put(ar.get(i), group.get(ar.get(i)) +1 );
-            }
+        HashMap<Integer, Integer> group = new HashMap<>();
+
+        for (int sock : ar) {
+            group.put(sock, group.getOrDefault(sock, 0) + 1);
         }
+
         int count = 0;
-        for (Integer integer : group.values()) {
-            count += (integer/2 > 0) ? (int) integer/2 : 0;
+        for (int quantity : group.values()) {
+            count += quantity / 2;
         }
+
         return count;
     }
 }
